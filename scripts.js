12c70d08ae8c8ada193136e112f7de9c6331d492
Sunnynobleman <mrnobleman2025@gmail.com>
2025-01-08 16:36:48 +0330

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
-var map = L.map("map", {
-    center: [35.745061, 51.213648],
-    zoom: 12,
-    preferCanvas: false,
+document.addEventListener("DOMContentLoaded", function () {
+    var map = L.map("map", {
+        center: [35.745061, 51.213648],
+        zoom: 12,
+        preferCanvas: false,
+    });
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: "&copy; OpenStreetMap contributors",
+    }).addTo(map);
+
+    var marker = L.marker([35.745061, 51.213648]).addTo(map);
+    marker.bindPopup("<b>Chitgar Lake</b><br>Coordinates: 35.745061, 51.213648");
 });
-
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "&copy; OpenStreetMap contributors",
-}).addTo(map);
-
-var marker = L.marker([35.745061, 51.213648]).addTo(map);
-marker.bindPopup("<b>Chitgar Lake</b><br>Coordinates: 35.745061, 51.213648");
